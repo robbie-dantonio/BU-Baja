72a22f9efda0c231ebcd9d09b89ec19c59b48167
Krish Shah <63075612+krish-shahh@users.noreply.github.com>
2023-03-15 19:49:57 -0400

    Update flow.ino

--- flow/flow.ino
+++ flow/flow.ino
@@ -1,29 +1,33 @@
-int flowPin = 2;  // input pin for the flow sensor
-float flowRate;   // flow rate in gal/min
+const int flowPin = 2;  // input pin for the flow sensor
+const float pulseFactor = 0.00066;  // volume of each pulse in gallons
+float flowRate;  // flow rate in gallons per minute
 float totalVolume; // total volume of liquid passed through the sensor in gallons
-unsigned long previousMillis = 0;
-unsigned long currentMillis;
-unsigned long elapsedTime;
-float calibrationFactor = 7.5;  // adjust this value according to the sensor's datasheet
+unsigned long startTime;
+unsigned int pulseCount;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(flowPin, INPUT);
+  pinMode(flowPin, INPUT_PULLUP);
+  pulseCount = 0;
+  startTime = millis();
 }
 
 void loop() {
-  currentMillis = millis();
-  elapsedTime = currentMillis - previousMillis;
-  if (elapsedTime > 1000) { // calculate flow rate every second
-    float sensorReading = pulseIn(flowPin, HIGH);  // read the sensor's output
-    flowRate = (sensorReading / calibrationFactor) * 60.0;  // convert to gal/min
-    totalVolume += (flowRate / 60.0) * elapsedTime / 3785.41;  // add the volume passed during this interval to the total (1 gallon = 3785.41 mL)
+  if (digitalRead(flowPin) == LOW) {
+    pulseCount++;  // increment the pulse count when a pulse is detected
+  }
+  unsigned long currentTime = millis();
+  unsigned long elapsedTime = currentTime - startTime;
+  if (elapsedTime >= 1000) {  // calculate flow rate every second
+    flowRate = pulseCount * pulseFactor * 60.0;  // calculate flow rate in gallons per minute
+    totalVolume += flowRate / 60.0;  // add the volume passed during this interval to the total
     Serial.print("Flow rate: ");
     Serial.print(flowRate, 2);
     Serial.print(" gal/min\t");
     Serial.print("Total volume: ");
     Serial.print(totalVolume, 2);
     Serial.println(" gallons");
-    previousMillis = currentMillis;
+    pulseCount = 0;  // reset the pulse count
+    startTime = currentTime;  // reset the start time
   }
 }
