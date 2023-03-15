7272d3decf58552b7ce243d206a97a5136a280a0
Krish Shah <63075612+krish-shahh@users.noreply.github.com>
2023-03-15 19:46:56 -0400

    Update flow.ino

--- flow/flow.ino
+++ flow/flow.ino
@@ -1,10 +1,10 @@
 int flowPin = 2;  // input pin for the flow sensor
-float flowRate;   // flow rate in L/min
-float totalVolume; // total volume of liquid passed through the sensor in L
+float flowRate;   // flow rate in gal/min
+float totalVolume; // total volume of liquid passed through the sensor in gallons
 unsigned long previousMillis = 0;
 unsigned long currentMillis;
 unsigned long elapsedTime;
-float calibrationFactor = 4.5;  // adjust this value according to the sensor's datasheet
+float calibrationFactor = 7.5;  // adjust this value according to the sensor's datasheet
 
 void setup() {
   Serial.begin(9600);
@@ -16,14 +16,14 @@ void loop() {
   elapsedTime = currentMillis - previousMillis;
   if (elapsedTime > 1000) { // calculate flow rate every second
     float sensorReading = pulseIn(flowPin, HIGH);  // read the sensor's output
-    flowRate = (sensorReading / calibrationFactor) * 60.0;  // convert to L/min
-    totalVolume += (flowRate / 60.0) * elapsedTime;  // add the volume passed during this interval to the total
+    flowRate = (sensorReading / calibrationFactor) * 60.0;  // convert to gal/min
+    totalVolume += (flowRate / 60.0) * elapsedTime / 3785.41;  // add the volume passed during this interval to the total (1 gallon = 3785.41 mL)
     Serial.print("Flow rate: ");
     Serial.print(flowRate, 2);
-    Serial.print(" L/min\t");
+    Serial.print(" gal/min\t");
     Serial.print("Total volume: ");
     Serial.print(totalVolume, 2);
-    Serial.println(" L");
+    Serial.println(" gallons");
     previousMillis = currentMillis;
   }
 }
