459940f7527fdeb384350843f24b2451e5cd1c28
robbiedantonio <robbie.dantonio@gmail.com>
2023-04-01 11:58:51 -0400

    updates

--- Arduino/StepperMotor/StepperMotor.ino
+++ Arduino/StepperMotor/StepperMotor.ino
@@ -12,9 +12,10 @@
 #define STEPS_PER_REVOLUTION 600
 
 /* instantiate Stepper class on pins 8, 9, 10, 11 */
-Stepper speedo(STEPS_PER_REVOLUTION, 8, 9, 10, 11); 
+Stepper speedo(STEPS_PER_REVOLUTION, 4, 5, 6, 7); 
 
-int stepCount = 0;	// Number of steps the motor has taken
+//int stepCount = 0;	// Number of steps the motor has taken
+//int gpsSpeed = 0; // Dummy gpsSpeed variable used for testing
 
 //speedo.setSpeed(500);	// Speed at which the motor rotates. This might be too fast, need to test!
 
@@ -25,7 +26,7 @@ void setup(){
 	 * 	- ^^ Maybe not necesary, need to test!
 	 */
 
-	// Set stepper motor speed to 60 RPM. Test different values to make sure this is ok!
+	// Set stepper motor speed to 30 RPM. Test different values to make sure this is ok!
 	speedo.setSpeed(60);
 }
 
@@ -37,12 +38,13 @@ void loop(){
 	 */
 
 	int gpsSteps = map(gpsSpeed, 0, 15, 0, 600); // Calculate how many steps correspond to current GPS Speed. Assumes max speed is 15, but need to confirm!
-
+  speedo.step(50);
 		
 	/* If GPS Speed is greater than motor position, step forward.
 	 * Else if GPS speed is less than position, step backward.
 	 * Else don't step!
 	 */
+  /*
 	if (gpsSteps > stepCount) { 		// If gpsSteps is higher, then step forward
 		speedo.step(gpsSteps - stepCount);
 		stepCount = stepCount + gpsSteps;	
@@ -50,6 +52,10 @@ void loop(){
 	else if (gpsSteps < stepCount) { 	// If gpsSteps is lower than current position, step backward
 		speedo.step(stepCount - gpsSteps);
 		stepCount = stepCount - gpsSteps;
+   delay(1000);
+   gpsSpeed++;
+   
 	}
 	// Otherwise, don't change position!	
+*/
 }
